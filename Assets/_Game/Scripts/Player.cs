2f8c637e76723ec6cc6ee60d50cf4014e6689eb8
CrazyTrunk <ncongtrung1610@gmail.com>
2023-10-13 16:31:49 +0700

    AddStack

--- Assets/_Game/Scripts/Player.cs
+++ Assets/_Game/Scripts/Player.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Singleton<Player>
 {
     [SerializeField] private float speed = 5f;
     private Direct currentDirection = Direct.None;
     [SerializeField] private LayerMask layerBrick;
     private bool isMoving = false;
     private Vector3 targetPosition;
+    [SerializeField] public GameObject stackBrickParent;
+    [SerializeField] public GameObject prevBrick;
+    [SerializeField] public GameObject skin;
 
     private void OnEnable()
     {
@@ -29,7 +33,7 @@ public class Player : MonoBehaviour
     }
     private void HandleMovePlayer(Direct direction)
     {
-        if (!isMoving  && direction != Direct.None) // Chỉ xử lý vuốt nếu nhân vật không đang di chuyển
+        if (!isMoving && direction != Direct.None) // Chỉ xử lý vuốt nếu nhân vật không đang di chuyển
         {
             currentDirection = direction;
             targetPosition = GetNextPoint(currentDirection);
@@ -41,7 +45,7 @@ public class Player : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
 
-        if (transform.position ==  targetPosition)
+        if (transform.position == targetPosition)
         {
             isMoving = false;
             currentDirection = Direct.None;
@@ -80,5 +84,15 @@ public class Player : MonoBehaviour
 
         return directionVector;
     }
-
+    public void AddBrick(GameObject currentBrickWhenCollide)
+    {
+        currentBrickWhenCollide.transform.SetParent(stackBrickParent.transform);
+        Vector3 posOfCurrentBrick = prevBrick.transform.localPosition;
+        currentBrickWhenCollide.transform.localPosition = posOfCurrentBrick;
+        posOfCurrentBrick.y += 0.5f;
+        prevBrick.transform.localPosition = posOfCurrentBrick;
+        Vector3 Characterskin = skin.transform.localPosition;
+        Characterskin.y += 0.5f;
+        skin.transform.localPosition = Characterskin;
+    }
 }
